2d675e777cc8f276dbe0d5fa343386c40ee5e9e4
developerjahed <developerjahed@gmail.com>
2021-12-17 21:27:00 +0600

    js updatet

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
-const memory16=document.getElementById('update-memory');
-memory16.addEventListener('click', function(){
-    const memoryCost=document.getElementById('memory-cost');
-    memoryCost.innerText='100';
+const state = {
+	basePrice: 1399,
+	exMemory: 100,
+	exStorage: 120,
+	fastDelivery: 20,
+};
+
+const totalPrice = document.getElementById('total-price');
+const memory16 = document.getElementById('update-memory');
+const deliveryCharge = document.getElementById('delivery-charge');
+const deliveryCost = document.getElementById('delivery-cost');
+const storage16 = document.getElementById('update-storage');
+const storageCost = document.getElementById('extra-storage');
+const memoryCost = document.getElementById('memory-cost');
+totalPrice.innerHTML = state.basePrice;
+
+memory16.addEventListener('click', function () {
+	memoryCost.innerText = state.exMemory;
+	state.basePrice += state.exMemory;
+	totalPrice.innerHTML = state.basePrice;
 });
 
-const storage16=document.getElementById('update-storage');
-storage16.addEventListener('click', function(){
-    const storageCost=document.getElementById('extra-storage');
-    storageCost.innerText='120';
+storage16.addEventListener('click', function () {
+	storageCost.innerText = state.exStorage;
+	state.basePrice += state.exStorage;
+	totalPrice.innerHTML = state.basePrice;
 });
-
-const deliveryCharge=document.getElementById('delivery-charge');
-deliveryCharge.addEventListener('click', function(){
-    const deliveryCost=document.getElementById('delivery-cost');
-    deliveryCost.innerText='120';
+deliveryCharge.addEventListener('click', function () {
+	deliveryCost.innerText = state.fastDelivery;
+	state.basePrice += state.fastDelivery;
+	totalPrice.innerHTML = state.basePrice;
 });
-
-
